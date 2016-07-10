09ff1c821704e77ea7477944ccb43fef9065808c
AeroSteveO <thesteveo64@gmail.com>
2016-07-10 00:13:16 -0500

    added webserver backup scripts

---
+++ Backup Scripts/backup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /var/www/html
+
+tar zcf /media/backup/Backup/Computers/Sites/dokuwiki-`date +%Y%m%d`.tar.gz dokuwiki
+
---
+++ Backup Scripts/backup_everything.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /var/www/
+
+tar zcf /media/backup/Backup/Computers/Sites/webserver-`date +%Y%m%d`.tar.gz html
+
