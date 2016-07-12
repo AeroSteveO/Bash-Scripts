5b59ca9b8c20594c963ae5d0c99cfe7f2bf4b373
AeroSteveO <thesteveo64@gmail.com>
2016-07-12 15:12:32 -0500

    comments

--- Backup-Scripts/backup.sh
+++ Backup-Scripts/backup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /var/www/html
 
+#specifically backup the dokuwiki website
 tar zcf /media/backup/Backup/Computers/Sites/dokuwiki-`date +%Y%m%d`.tar.gz dokuwiki
 
--- Backup-Scripts/backup_everything.sh
+++ Backup-Scripts/backup_everything.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /var/www/
 
+#basic backup of the var/www directory to a mounted network drive
 tar zcf /media/backup/Backup/Computers/Sites/webserver-`date +%Y%m%d`.tar.gz html
 
