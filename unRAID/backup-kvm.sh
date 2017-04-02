038c0fca6d4fe26767fef93e0af8b2d6e159e128
AeroSteveO <thesteveo64@gmail.com>
2017-04-02 17:52:33 -0500

    added additional logic for creating backup folders

--- unRAID/backup-kvm.sh
+++ unRAID/backup-kvm.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 list=$(virsh list --name)
-list=$(echo $list | awk '{print tolower($0)}')
+#list=$(echo $list | awk '{print tolower($0)}')
 backupDir=/mnt/user/Backup/VM
+
+# Setup directories needed
+if [ ! -d $backupDir/`date +%Y%m%d` ]; then
+  # Control will enter here if $DIRECTORY doesn't exist.
+  mkdir $backupDir/`date +%Y%m%d`
+fi
+
+
 
 
 for VM_NAME in $list
 do
+
+LC_VM_NAME=$(echo $VM_NAME | awk '{print tolower($0)}')
+
+echo $VM_NAME
+echo $LC_VM_NAME
+
+
 # Freeze guest filesystems
 virsh domfsfreeze $VM_NAME
 
